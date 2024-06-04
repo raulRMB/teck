4e086dc70c389dff83cdd69450e1fa259ef550e4
raul <raulrmbdev@gmail.com>
2024-06-04 15:39:35 -0400

    Triangle

--- cmake-build-debug/_deps/glm-build/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raul/dev/jet/cmake-build-debug/_deps/glm-src/glm
-# Build directory: /home/raul/dev/jet/cmake-build-debug/_deps/glm-build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-release/CPM_modules/Findglfw.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/raul/dev/jet/cmake-build-release/cmake/CPM.cmake")
-CPMAddPackage("NAME;glfw;GITHUB_REPOSITORY;glfw/glfw;GIT_TAG;3.4")
-set(glfw_FOUND TRUE)
